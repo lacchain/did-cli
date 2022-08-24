7a10a40a09c759ac3de130d58c85b52ff48f3375
Eduardo Marchena <edumar111@gmail.com>
2022-08-24 13:18:14 -0500

    config to gas model local

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
-export default {
-  network: 'main',
-  rpcUrl: 'https://writer.lacchain.net',
-  registry: '0xCC77A5e709cB473F49c943D9b40B989f986E5F2F',
-};
+// export default {
+//   network: 'main',
+//   rpcUrl: 'https://writer.lacchain.net',
+//   registry: '0xCC77A5e709cB473F49c943D9b40B989f986E5F2F',
+// };
 
 /*
 export default {
@@ -13,3 +13,12 @@ export default {
   rpcUrl: 'http://34.69.22.82'
 };
 */
+
+
+export default {
+  network: 'local',
+  registry: '0xffE05B674E1878B391f49547DA6F99CE102c8678',
+  nodeAddress: '0x211152ca21d5daedbcfbf61173886bbb1a217242',
+  expiration: 1836394529,
+  rpcUrl: 'http://localhost:4545'
+};
